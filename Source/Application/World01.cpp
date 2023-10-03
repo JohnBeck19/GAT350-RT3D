4910230d2b46880f90b29a5e6f8741be33dafb7c
JohnBeck19 <jbeck@student.neumont.edu>
2023-10-03 17:36:41 -0600

    needles

--- Source/Application/World01.cpp
+++ Source/Application/World01.cpp
@@ -7,6 +7,10 @@ namespace nc
 {
     bool World01::Initialize()
     {
+        for (int i = 0; i < 10; i++)
+        {
+            m_pos.push_back(vec2{ randomf(-0.25f, 0.25f), randomf(-0.25f, 0.25f) });
+        }
         return true;
     }
 
@@ -16,6 +20,21 @@ namespace nc
 
     void World01::Update(float dt)
     {
+
+        m_angle += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_Q) ? -dt*360 : 0;
+        m_angle += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_E) ? +dt*360 : 0;
+        for (int i = 0; i < 10; i++) {
+
+            m_pos[i].x += dt * random(-1, 1);
+            m_pos[i].y += dt * random(-1, 1);
+            m_pos[i].x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_A) ? -dt : 0;
+            m_pos[i].x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_D) ? +dt : 0;
+            m_pos[i].y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_S) ? -dt : 0;
+            m_pos[i].y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_W) ? +dt : 0;
+
+        }
+        m_time += dt;
+
     }
 
     void World01::Draw(Renderer& renderer)
@@ -24,10 +43,28 @@ namespace nc
 
         renderer.BeginFrame();
 
-        // render
-        glBegin(GL_LINE);
+        for (int i = 0; i < 10; i++) {
+            // render
+            glPushMatrix();
+            glTranslatef(m_pos[i].x, m_pos[i].y, 0);
+            glRotatef(m_angle, 1, 1, 1);
+            glScalef(sin((m_time * 5) + 1) * 0.2f, 1, 1);
 
+            glBegin(GL_TRIANGLES);
 
+            glColor3f(1, 0, 1);
+            glVertex2f(-0.25f, -0.25f);
+
+            glColor3f(1, 1, 1);
+            glVertex2f(0, 0.25f);
+
+            glColor3f(0, 1, 1);
+            glVertex2f(0.25f, -0.25f);
+
+            glEnd();
+
+        glPopMatrix();
+        }
         // post-render
         renderer.EndFrame();
     }
--- Source/Application/World01.h
+++ Source/Application/World01.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "Framework/World.h"
 #include "Core/Math/Vector2.h"
-
+#include <vector>
 namespace nc
 {
 	class World01 : public World
@@ -16,5 +16,6 @@ namespace nc
 		float m_angle = 0;
 		float m_time;
 		vec2 m_position;
+		std::vector<vec2> m_pos;
 	};
 }
